77920a48818cf230c62587dcea0b0a7c7f10b9e9
adfoke <trawow@gmail.com>
2025-02-10 07:44:49 +0800

    make the main file

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # epark
 c++ lang for fun
+
+#build
+mkdir build && cd build
+cmake ..
+make
+
+#run
+./epark_main
---
+++ cmd/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "hello.h"
+
+int main() {
+    std::cout << "Welcome to epark!" << std::endl;
+    sayHello();
+    return 0;
+}
